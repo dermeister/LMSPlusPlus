80e2c763c63097adba48a3f0cacc2a022e0eff69
Denis Duzh <duzh.denis@gmail.com>
2021-07-21 22:53:10 +0300

    feat: explorer reconciliation

--- client/src/models/explorer/Explorer.ts
+++ client/src/models/explorer/Explorer.ts
@@ -1,13 +1,48 @@
-import { cached, Transaction } from "reactronic"
+import { cached, transaction, Transaction, unobservable } from "reactronic"
 import { ObservableObject } from "../../ObservableObject"
+import { ExplorerReconciler } from "./ExplorerReconciler"
+import { GroupNode } from "./GroupNode"
 import { ItemNode } from "./ItemNode"
+import { Node } from "./Node"
+
+class ExplorerRootNode extends GroupNode {
+  override get isOpened(): boolean { return true }
+
+  constructor(children: readonly Node[]) {
+    super("Explorer", "explorer", children)
+  }
+
+  @transaction
+  updateExplorerRootNode(children: readonly Node[]): void { this.updateGroupNode(this.title, children) }
+}
 
 export class Explorer<T> extends ObservableObject {
-  private _activeNode: ItemNode<T> | null = null
+  @unobservable readonly root: ExplorerRootNode
+  @unobservable private readonly reconciler: ExplorerReconciler<this, T> = new ExplorerReconciler(this)
+  private _selectedNode: ItemNode<T> | null = null
 
-  @cached get activeNode(): ItemNode<T> | null { return this._activeNode }
+  @cached get children(): readonly Node[] { return this.root.children }
+  @cached get selectedNode(): ItemNode<T> | null { return this._selectedNode }
 
-  activateNode(node: ItemNode<T>): void {
-    Transaction.run(() => this._activeNode = node)
+  constructor(children: readonly Node[] = []) {
+    super()
+    this.root = new ExplorerRootNode(children)
+    this.reconciler.reconcile()
+  }
+
+  @transaction
+  updateExplorer(children: readonly Node[]): void {
+    this.root.updateExplorerRootNode(children)
+    this.reconciler.reconcile()
+  }
+
+  @transaction
+  setSelectedNode(node: ItemNode<T> | null): void { this._selectedNode = node }
+
+  override dispose(): void {
+    Transaction.run(() => {
+      this.reconciler.dispose()
+      super.dispose()
+    })
   }
 }
